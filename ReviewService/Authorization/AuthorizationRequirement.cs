429eb340b47640a70aa82b7b3abd122f755400fc
Dragan Mirkovic <63748840+Dragan2402@users.noreply.github.com>
2024-08-20 22:50:55 +0200

    Feature 4: Added authorization flow (#5)

---
+++ ReviewService/Authorization/AuthorizationRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace ReviewService.Authorization;
+
+public class AuthorizationRequirement : IAuthorizationRequirement
+{
+    public AuthorizationLevel AuthorizationLevel { get; set; }
+
+    public AuthorizationRequirement(AuthorizationLevel permission)
+    {
+        AuthorizationLevel = permission;
+    }
+}
